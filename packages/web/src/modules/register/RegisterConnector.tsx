51154fe52fa84636c961fb1c35bb767fa83d9877
manojkmishra <manojmishra571980@gmail.com>
2018-07-03 14:35:05 +1000

    RegisterView created

--- packages/web/src/modules/register/RegisterConnector.tsx
+++ packages/web/src/modules/register/RegisterConnector.tsx
@@ -1,3 +1,6 @@
 import * as React from "react";
+import { RegisterView } from "./ui/RegisterView";
 
-export const RegisterConnector = () => <div>register page</div>;
+// export const RegisterConnector = () => <div>register page</div>;
+
+export const RegisterConnector = () => <RegisterView/>;
---
+++ packages/web/src/modules/register/ui/RegisterView.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+
+export class RegisterView extends React.PureComponent 
+{
+ render() { return (<div>hello</div>) ;}
+}
