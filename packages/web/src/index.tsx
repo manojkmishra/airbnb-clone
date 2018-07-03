6eeda9c4c030abf7dca4500e3b177cffd379591d
manojkmishra <manojmishra571980@gmail.com>
2018-07-03 14:00:48 +1000

    react-router-dom installed --register module page opening

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,15 +1,14 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { ApolloProvider } from "react-apollo";
-import App from './App';
 
 import registerServiceWorker from "./registerServiceWorker";
 import { client } from "./apollo";
-// import { Routes } from "./routes";
+import { Routes } from "./routes";
 
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <App />
+    <Routes/>
   </ApolloProvider>,
   document.getElementById("root") as HTMLElement
 );
