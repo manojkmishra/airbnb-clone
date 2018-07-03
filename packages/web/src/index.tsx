a1f2554f4f4587f0e04ffaac886163316fb6f0c1
manojkmishra <manojmishra571980@gmail.com>
2018-07-03 13:38:17 +1000

    apolloClient setup for react web app done

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,10 +1,16 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import { ApolloProvider } from "react-apollo";
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
+import registerServiceWorker from "./registerServiceWorker";
+import { client } from "./apollo";
+// import { Routes } from "./routes";
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root") as HTMLElement
 );
 registerServiceWorker();
