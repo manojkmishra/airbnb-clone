4dbb37d135e7a5ce3c17285a7b9704abef7f1e21
manojkmishra <manojmishra571980@gmail.com>
2018-07-05 11:55:01 +1000

    Formik fields in separate file then imported in form

--- packages/web/src/modules/register/ui/RegisterView.tsx
+++ packages/web/src/modules/register/ui/RegisterView.tsx
@@ -1,53 +1,49 @@
 import * as React from "react";
-import { Form, Icon, Input, Button } from "antd";
-import { withFormik, FormikErrors, FormikProps } from "formik";
-// import * as yup from "yup";
+import * as Antd from "antd";
+import { withFormik, FormikErrors, FormikProps, Field, Form } from "formik";
 import { validUserSchema } from "@airbnb/common";
+import { InputField } from "../../shared/InputField";
 
-const FormItem = Form.Item;
+const { Form: AntForm, Icon, Button } = Antd;
+const FormItem = AntForm.Item;
 
-interface FormValues {  email: string;  password: string;}
+interface FormValues {
+  email: string;
+  password: string;
+}
 
-interface Props {  submit: (values: FormValues) => Promise<FormikErrors<FormValues> | null>;}
+interface Props {
+  submit: (values: FormValues) => Promise<FormikErrors<FormValues> | null>;
+}
 
-class C extends React.PureComponent<FormikProps<FormValues> & Props> 
-{  render() 
-    { const { values, handleChange, handleBlur, handleSubmit, touched, errors } = this.props;
-              console.log('/src/modules/register/ui/registerview.tsx--this.props=',this.props);
-              console.log('/src/modules/register/ui/registerview.tsx--errors=',errors);
-      return (
-      <form style={{ display: "flex" }} onSubmit={handleSubmit}>
+class C extends React.PureComponent<FormikProps<FormValues> & Props> {
+  render() {
+    console.log('/src/modules/register/ui/registerview.tsx--this.props=',this.props);
+    
+    return (
+      <Form style={{ display: "flex" }}>
         <div style={{ width: 400, margin: "auto" }}>
-          <FormItem
-            help={touched.email && errors.email ? errors.email : ""}
-            validateStatus={touched.email && errors.email ? "error" : undefined}
-          >
-            <Input
-              name="email"
-              prefix={<Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />}
-              placeholder="Email"
-              value={values.email}
-              onChange={handleChange}
-              onBlur={handleBlur}
-            />
-          </FormItem>
-          <FormItem
-            help={touched.password && errors.password ? errors.password : ""}
+          <Field
+            name="email"
             // tslint:disable-next-line:jsx-no-multiline-js
-            validateStatus={
-              touched.password && errors.password ? "error" : undefined
+            prefix={
+              <Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} /> as any
+              // tslint:disable-next-line:jsx-curly-spacing
             }
-          >
-            <Input
-              name="password"
-              prefix={<Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />}
-              type="password"
-              placeholder="Password"
-              value={values.password}
-              onChange={handleChange}
-              onBlur={handleBlur}
-            />
-          </FormItem>
+            placeholder="Email"
+            component={InputField}
+          />
+          <Field
+            name="password"
+            type="password"
+            // tslint:disable-next-line:jsx-no-multiline-js
+            prefix={
+              <Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} /> as any
+              // tslint:disable-next-line:jsx-curly-spacing
+            }
+            placeholder="Password"
+            component={InputField}
+          />
           <FormItem>
             <a className="login-form-forgot" href="">
               Forgot password
@@ -66,33 +62,13 @@ class C extends React.PureComponent<FormikProps<FormValues> & Props>
             Or <a href="">login now!</a>
           </FormItem>
         </div>
-      </form>
+      </Form>
     );
   }
 }
 
-/*
-const emailNotLongEnough = "email must be at least 3 characters";
-const passwordNotLongEnough = "password must be at least 3 characters";
-const invalidEmail = "email must be a valid email";
-const validationSchema = yup.object().shape({
-  email: yup
-    .string()
-    .min(3, emailNotLongEnough)
-    .max(255)
-    .email(invalidEmail)
-    .required(),
-  password: yup
-    .string()
-    .min(3, passwordNotLongEnough)
-    .max(255)
-    .required()
-});
-*/
-
 export const RegisterView = withFormik<Props, FormValues>({
- // validationSchema,
- validationSchema: validUserSchema,
+  validationSchema: validUserSchema,
   mapPropsToValues: () => ({ email: "", password: "" }),
   handleSubmit: async (values, { props, setErrors }) => {
     const errors = await props.submit(values);
---
+++ packages/web/src/modules/shared/InputField.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { FieldProps } from "formik";
+import { Form, Input } from "antd";
+
+const FormItem = Form.Item;
+// copied from https://github.com/jaredpalmer/formik#field-
+export const InputField: React.SFC< FieldProps<any> & { prefix: React.ReactNode } > = 
+({
+  field, // { name, value, onChange, onBlur }
+  form: { touched, errors }, // also values, setXXXX, handleXXXX, dirty, isValid, status, etc.
+  ...props
+}) => {  const errorMsg = touched[field.name] && errors[field.name];
+   
+    console.log('/src/modules/register/shared/inputfield.tsx--errors=',errors);
+         return (
+                    <FormItem help={errorMsg} validateStatus={errorMsg ? "error" : undefined}>
+                    <Input {...field} {...props} />
+                    </FormItem>
+                );
+      };
