43cf6306616a11cff38e5e0e806de18190b36748
manojkmishra <manojmishra571980@gmail.com>
2018-07-06 11:32:15 +1000

    apollo-codegen---introscpect---generate

--- packages/controller/src/modules/RegisterController/index.tsx
+++ packages/controller/src/modules/RegisterController/index.tsx
@@ -2,13 +2,15 @@
 import * as React from "react";
 import { graphql, ChildMutateProps } from "react-apollo";
 import gql from "graphql-tag";
+import {  RegisterMutation,  RegisterMutationVariables} from "./__generated__/RegisterMutation";
 
-interface Props {  children: (    data: { submit: (values: any) => Promise<null> }  ) => JSX.Element | null; }
+interface Props {  children: (    data: { submit: (values:RegisterMutationVariables) => Promise<null> }  ) => JSX.Element | null; }
 
   // export class RegisterController extends React.PureComponent<Props> 
-class C extends React.PureComponent<ChildMutateProps<Props, any, any>> 
+// class C extends React.PureComponent<ChildMutateProps<Props, any, any>> 
+class C extends React.PureComponent< ChildMutateProps<Props, RegisterMutation, RegisterMutationVariables>>
 {
-    submit = async (values: any) => 
+    submit = async (values: RegisterMutationVariables) => 
     {
       console.log('/controller/src/modules/regcontroller/index.tsx-values=',values);
       const response = await this.props.mutate({   variables: values });
@@ -21,9 +23,10 @@ class C extends React.PureComponent<ChildMutateProps<Props, any, any>>
     }
 }
 const registerMutation = gql`
-  mutation($email: String!, $password: String!) 
+  mutation RegisterMutation($email: String!, $password: String!) 
   {    register(email: $email, password: $password) {  path  message  }
   }
 `;
 
-export const RegisterController = graphql(registerMutation)(C);
+// export const RegisterController = graphql(registerMutation)(C);
+export const RegisterController = graphql<Props,RegisterMutation, RegisterMutationVariables>(registerMutation)(C);
