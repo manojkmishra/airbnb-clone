a239160c521d12549238df7e7b234a626354d4dc
manojkmishra <manojmishra571980@gmail.com>
2018-07-08 15:02:13 +1000

    changes for deploying on heroku

--- deploy_server_do.sh
+++ deploy_server_do.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 yarn build:server
-docker build -t manojkmishra/airbnb:latest .
-docker push manojkmishra/airbnb:latest
-ssh root@167.99.11.233 "docker pull manojkmishra/airbnb:latest && docker tag manojkmishra/airbnb:latest dokku/airbnb:latest && dokku tags:deploy airbnb latest"
+heroku container:push --app=frozen-brook-28503 web
+heroku container:release --app=frozen-brook-28503 web
+#docker build -t manojkmishra/airbnb:latest .
+#docker push manojkmishra/airbnb:latest
+#ssh root@178.128.146.169 "docker pull manojkmishra/airbnb:latest && docker tag manojkmishra/airbnb:latest dokku/airbnb:latest && dokku tags:deploy airbnb latest"
