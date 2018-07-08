4a6a7cfb332c481a3c166ed7eaab396c3410324d
manojkmishra <manojmishra571980@gmail.com>
2018-07-08 13:24:55 +1000

    server modified because - doku register not working

--- packages/server/src/utils/createTypeormConn.ts
+++ packages/server/src/utils/createTypeormConn.ts
@@ -1,4 +1,5 @@
 import { getConnectionOptions, createConnection } from "typeorm";
+import { User } from "../entity/User";
 
 export const createTypeormConn = async () => {
   const connectionOptions = await getConnectionOptions(process.env.NODE_ENV);
@@ -7,6 +8,7 @@ export const createTypeormConn = async () => {
     ? createConnection({
         ...connectionOptions,
         url: process.env.DATABASE_URL,
+        entities: [User],
         name: "default"
       } as any)
 : createConnection({ ...connectionOptions, name: "default" });
