ae74d52ad03f6546e0fd1e914faa7300b4d898b9
manojkmishra <manojmishra571980@gmail.com>
2018-07-12 07:25:15 +1000

    nodemailer- send mail working

--- packages/server/src/utils/sendEmail.ts
+++ packages/server/src/utils/sendEmail.ts
@@ -1,25 +1,35 @@
-import * as SparkPost from "sparkpost";
-const client = new SparkPost(process.env.SPARKPOST_API_KEY1);
-console.log('start email send=1');
+import * as nodemailer from "nodemailer";
 
-export const sendEmail = async (recipient: string, url: string) => 
-{
-  console.log('start email send=recepient', recipient);
-  console.log('start email send=url', url);
-  const response = await client.transmissions.send(
-    {
-      options: {  sandbox: true  },
-      content: {
-        from: "testing@sparkpostbox.com",
-        subject: "Confirm Email",
-        html: `<html>
-          <body>
-          <p>Testing SparkPost - the world's most awesomest email service!</p>
-          <a href="${url}">confirm email</a>
-          </body>
-          </html>`
-    },
-    recipients: [{ address: recipient }]
+export const sendEmail = async (recipient: string, url: string) => {
+  const transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    auth: {
+      user: process.env.NODEMAILER_EMAIL,
+      pass: process.env.NODEMAILER_PASSWORD
+    }
   });
-  console.log('server/src/utils/sendemail.ts-response=',response);
+
+  const message = {
+    from: "Sender Name <sender@example.com>",
+    to: `Recipient <${recipient}>`,
+    subject: "Nodemailer is unicode friendly ✔",
+    text: "Hello to myself!",
+    html: `<html>
+        <body>
+        <p>Testing SparkPost - the world's most awesomest email service!</p>
+        <a href="${url}">confirm email</a>
+        </body>
+        </html>`
+  };
+
+  transporter.sendMail(message, (err, info) => {
+    if (err) {
+      console.log("Error occurred. " + err.message);
+    }
+
+    console.log("Message sent: %s", info.messageId);
+    // Preview only available when sending through an Ethereal account
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  });
 };
