192764c8a0482c308e8cb6d16f3e29c7773a010c
manojkmishra <manojmishra571980@gmail.com>
2018-07-12 08:37:16 +1000

    RN- login connector connected with controllers

--- packages/app/src/modules/login/LoginConnector.tsx
+++ packages/app/src/modules/login/LoginConnector.tsx
@@ -1,11 +1,15 @@
 import * as React from "react";
-
+import { LoginController } from "@airbnb/controller";
 import { LoginView } from "./ui/LoginView";
 
 export class LoginConnector extends React.PureComponent {
-   dummySubmit = async (values: any) => {    console.log('app/login/loginconnector.tsx-values=',values);    return null;  };
-  render() 
-  {  return <LoginView submit={this.dummySubmit}/>;
-
-  }
+ //  dummySubmit = async (values: any) => {    console.log('app/login/loginconnector.tsx-values=',values);    return null;  };
+ // render()   {//  return <LoginView submit={this.dummySubmit}/>; }
+  render() {
+    return (
+      <LoginController>
+        {({ submit }) => <LoginView submit={submit} />}
+      </LoginController>
+    );
+}
 }
