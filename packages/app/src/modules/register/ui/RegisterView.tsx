2b243d9b8919567c1d26cc20910051d9bc1e4a23
manojkmishra <manojmishra571980@gmail.com>
2018-07-11 11:34:05 +1000

    app login-- flex and card added, autocapitalize=off

--- packages/app/src/modules/register/ui/RegisterView.tsx
+++ packages/app/src/modules/register/ui/RegisterView.tsx
@@ -1,8 +1,9 @@
 import * as React from "react";
 import { withFormik, FormikErrors, FormikProps, Field } from "formik";
 import { validUserSchema } from "@airbnb/common";
-import { View, Button } from "react-native";
+import { View,Text } from "react-native";
 import { InputField } from "../../shared/InputField";
+import { Card, Button } from "react-native-elements";
 
 interface FormValues {  email: string;  password: string;}
 interface Props {  submit: (values: FormValues) => Promise<FormikErrors<FormValues> | null>;}
@@ -10,15 +11,30 @@ class C extends React.PureComponent<FormikProps<FormValues> & Props> {
   render() {
     const { handleSubmit } = this.props;
     return (
-      <View style={{ marginTop: 200 }}>
-        <Field name="email" placeholder="Email" component={InputField} />
-        <Field
-          name="password"
-          secureTextEntry={true}
-          placeholder="Password"
-          component={InputField}
-        />
-        <Button title="Submit" onPress={handleSubmit as any} />
+      <View style={{ flex: 1,  display: "flex", justifyContent: "center", backgroundColor: "skyblue" }}>
+        <Card>
+          <Text style={{ fontSize: 30, marginBottom: 10 }}>Register</Text>
+          <Field
+            name="email"
+            placeholder="Email"
+            component={InputField}
+            containerStyle={{ width: "100%" }}
+            autoCapitalize="none"
+          />
+          <Field
+            name="password"
+            secureTextEntry={true}
+            placeholder="Password"
+            component={InputField}
+            containerStyle={{ width: "100%" }}
+            autoCapitalize="none"
+          />
+          <Button
+            style={{ marginTop: 30 }}
+            title="Submit"
+            onPress={handleSubmit as any}
+          />
+        </Card>
       </View>
     );
   }
