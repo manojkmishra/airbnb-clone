27e95448d26c418af0b5cb85276e2df56aca3c62
manojkmishra <manojmishra571980@gmail.com>
2018-07-12 08:58:25 +1000

    server-reg resolver--send email removed------confirmed:false for user

--- packages/server/src/modules/user/register/resolvers.ts
+++ packages/server/src/modules/user/register/resolvers.ts
@@ -3,22 +3,23 @@ import { User } from "../../../entity/User";
 import { formatYupError } from "../../../utils/formatYupError";
 import {  duplicateEmail,} from "./errorMessages";
 import { validUserSchema } from "@airbnb/common";
-import { createConfirmEmailLink } from "./createConfirmEmailLink";
- import { sendEmail } from "../../../utils/sendEmail";
+// import { createConfirmEmailLink } from "./createConfirmEmailLink";
+// import { sendEmail } from "../../../utils/sendEmail";
 
 
 export const resolvers: ResolverMap = 
 {  Mutation: 
-    {   register: async (  _, args: GQL.IRegisterOnMutationArguments, { redis, url }  ) => 
+    {   register: async (  _, args: GQL.IRegisterOnMutationArguments,// { redis, url }  
+       ) => 
        {  try {  await validUserSchema.validate(args, { abortEarly: false });
               } catch (err) { return formatYupError(err);   }
          const { email, password } = args;
          const userAlreadyExists = await User.findOne({ where: { email }, select: ["id"] });
          if (userAlreadyExists) { return [ { path: "email", message: duplicateEmail } ]; }
-         const user = User.create({ email,  password });
+         const user = User.create({ email,  password, confirmed:false });
          await user.save();
-         if (process.env.NODE_ENV !== "test") 
-           { await sendEmail(  email,  await createConfirmEmailLink(url, user.id, redis)   );   }
+         // if (process.env.NODE_ENV !== "test") 
+          // { await sendEmail(  email,  await createConfirmEmailLink(url, user.id, redis)   );   }
          return null;
        }
   }
