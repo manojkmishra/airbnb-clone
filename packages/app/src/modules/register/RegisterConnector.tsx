81e35f8b88f5e1b48cfe00954fe86f210b6b5624
manojkmishra <manojmishra571980@gmail.com>
2018-07-11 11:45:18 +1000

    controller connected-----register done---confirmation email send via sparkpost

--- packages/app/src/modules/register/RegisterConnector.tsx
+++ packages/app/src/modules/register/RegisterConnector.tsx
@@ -1,12 +1,15 @@
 import * as React from "react";
 // import { Button } from "react-native-elements";
 import { RegisterView } from "./ui/RegisterView";
+import { RegisterController } from "@airbnb/controller";
 
 export class RegisterConnector extends React.PureComponent 
-{ dummySubmit = async (values: any) => {    console.log('/modules/reg/regconnector.tsx-values=',values);    return null;  };
+{ // dummySubmit = async (values: any) => {    console.log('/modules/reg/regconnector.tsx-values=',values);    return null;  };
  // onPress = () => {    console.log("button pressed");  };
   render() {
    // return <Button title="BUTTON" onPress={this.onPress} />;
-    return <RegisterView submit={this.dummySubmit} />;
+   return(<RegisterController>
+    {({ submit }) => <RegisterView submit={submit} />}
+    </RegisterController>);  
   }
 }
