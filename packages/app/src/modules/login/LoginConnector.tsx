6a118adf6b64e804e27d31fac384cb00359564d1
manojkmishra <manojmishra571980@gmail.com>
2018-07-12 10:59:21 +1000

    app- saving session id

--- packages/app/src/modules/login/LoginConnector.tsx
+++ packages/app/src/modules/login/LoginConnector.tsx
@@ -1,13 +1,16 @@
 import * as React from "react";
 import { LoginController } from "@airbnb/controller";
 import { LoginView } from "./ui/LoginView";
+import { SecureStore } from "expo";
+import { SID_KEY } from "../shared/constants";
 
 export class LoginConnector extends React.PureComponent {
  //  dummySubmit = async (values: any) => {    console.log('app/login/loginconnector.tsx-values=',values);    return null;  };
  // render()   {//  return <LoginView submit={this.dummySubmit}/>; }
+ saveSessionId = (sid: string) => {  SecureStore.setItemAsync(SID_KEY, sid);};
   render() {
     return (
-      <LoginController>
+      <LoginController onSessionId={this.saveSessionId}>
         {({ submit }) => <LoginView submit={submit} />}
       </LoginController>
     );
