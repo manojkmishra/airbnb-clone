749068b91498436ad333046dae204924dd974549
manojkmishra <manojmishra571980@gmail.com>
2018-07-16 15:55:52 +1000

    common comp imported in reg via router--so after reg--go back and confirm your emial page

--- packages/web/src/modules/register/RegisterConnector.tsx
+++ packages/web/src/modules/register/RegisterConnector.tsx
@@ -1,23 +1,25 @@
 import * as React from "react";
 import { RegisterView } from "./ui/RegisterView";
 import { RegisterController } from "@airbnb/controller";
+import { RouteComponentProps } from "react-router-dom";
 
-// container -> view
-// container -> connector -> view
-// controller -> connector -> view
-export class RegisterConnector extends React.PureComponent {
- // dummySubmit = async (values: any) => {
- //   console.log('/src/modules/register/registerconnector.tsx--dummysubmit()-values=',values);
- //   return null;
- // };
+
+// export class RegisterConnector extends React.PureComponent 
+export class RegisterConnector extends React.PureComponent<RouteComponentProps<{}>>
+{
+  onFinish = () => {
+    this.props.history.push("/m/confirm-email", {
+      message: "check your email to confirm your account"
+    });
+};
   render() {    console.log('web/src/modules/register/registerconnector.tsx--this.props=',this.props);
    // return <RegisterView submit={this.dummySubmit} />;
-   return (              
-              <RegisterController >
-              {({ submit }: { submit: any } ) => <RegisterView submit={submit} />}
-              </RegisterController>
-         
-          );
-    
-  }
+   return (
+    <RegisterController>
+      {({ submit }) => (
+        <RegisterView onFinish={this.onFinish} submit={submit} />
+      )}
+    </RegisterController>
+  );
+}
 }
