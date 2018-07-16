c7c3b2de90b3bf830f89cba17e1ab44862be5602
manojkmishra <manojmishra571980@gmail.com>
2018-07-16 16:09:36 +1000

    web- forgotpass----page geting fw to --check email and reset pass

--- packages/web/src/modules/forgotPassword/ForgotPasswordConnector.tsx
+++ packages/web/src/modules/forgotPassword/ForgotPasswordConnector.tsx
@@ -1,15 +1,20 @@
 import * as React from "react";
 import { ForgotPasswordView } from "./ui/ForgotPasswordView";
 import { ForgotPasswordController } from "@airbnb/controller";
+import { RouteComponentProps } from "react-router-dom";
 
-export class ForgotPasswordConnector extends React.PureComponent 
+export class ForgotPasswordConnector extends React.PureComponent <RouteComponentProps<{}>> 
 { // dummy = async (values: any) =>  {  console.log('/web/forgotpassconnector-values=',values);       return null;    };
-
+onFinish = () => {
+  this.props.history.push("/m/reset-password", {
+    message: "check your email to reset your password"
+  });
+};
   render() 
   {  // return <ForgotPasswordView submit={this.dummy} />;
   return (
     <ForgotPasswordController>
-      {({ submit }) => <ForgotPasswordView submit={submit} />}
+      {({ submit }) => <ForgotPasswordView onFinish={this.onFinish} submit={submit} />}
     </ForgotPasswordController>
 );
   }
